4e1f0a7767c976e491294e47f45d7eb568c8f230
Gaurav Gassi <gauravgassi123@gmail.com>
2022-08-16 10:52:33 +0530

    office

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 module.exports.init=function()
 {
   const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/tic-tac-toe')
+  mongoose.connect('mongodb+srv://root:root123@cluster0.rib2c.mongodb.net/tic-tac-toe?retryWrites=true&w=majority')
+
 .then(function()            
 {
   console.log("mongo is connected ")
